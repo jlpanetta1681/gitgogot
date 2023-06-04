95a09e9fdce786ce73be49c77f89d3df58759a51
Joe Panetta <jlpanetta1681@icloud.com>
2023-06-04 10:14:46 -0400

    setup: checks to be sure the project was added to the github repo correctly and completely

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
+// node index.js from this projects root directory to run.
+
 const jsonfile = require('jsonfile');
-const moment = require ('moment')
-const simpleGit = require('simple-git')
+const moment = require ('moment');
+const simpleGit = require('simple-git');
 const random = require('random');
 const FILE_PATH = './data.json';
 
-
+const getRandomDate = () => {
+  const startDate = moment('2022-11-11', 'YYYY-MM-DD');
+  const today = moment().startOf('day');
+  const diffDays = today.diff(startDate, 'days');
+  const randomDays = random.int(0, diffDays);
+  return startDate.add(randomDays, 'days');
+};
 
 const easyComm = n => {
-    if(n === 0) return simpleGit().push();
-    const x = random.int(0, 13);
-    const y = random.int(0,6);
-    const DATE = moment().subtract(13, 'w').add(1, 'd')
-              .add(x, 'w').add(y, 'd').format();
-    
-    const data = {
-        date: DATE
-    }
-    console.log(DATE)
-    jsonfile.writeFile(FILE_PATH, data, () => {
-        simpleGit().add([FILE_PATH]).commit(DATE,{'--date': DATE },
-        easyComm.bind(this, --n));
-    });
-}
+  if (n === 0) return simpleGit().push();
+  const DATE = getRandomDate().format();
+
+  const data = {
+    date: DATE
+  };
+
+  console.log(DATE);
+  
+  jsonfile.writeFile(FILE_PATH, data, () => {
+    simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE }, easyComm.bind(this, --n));
+  });
+};
+
 easyComm(500);
-    
